43986c7a6e52984bca3d40875e7f9b147a4c7529
deSoos <fsousa2004@gmail.com>
2023-11-17 17:57:27 +0000

    json stuff

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,17 +1,62 @@
+function validateForm() {
+    let nome1 = document.getElementById('nome1').value
+    let sobrenome2 = document.getElementById('sobrenome2').value
+    let idade = document.getElementById('idade').value
+    let genero1 = document.getElementById('Genero1').value
+    let genero2 = document.getElementById('Genero2').value
+    let genero3 = document.getElementById('Genero3').value
+    let genero4 = document.getElementById('Genero4').value
+    let rock_gostar = document.getElementsByName('rock_gostar').value
+    let metal_gostar = document.getElementsByName('metal-gostar').value
+    let ouvir_semana = document.getElementsByName('ouvir_semana').value
+
+    if (!nome1 || !sobrenome2 || !idade || !genero1 || !genero2 || !genero3 || !genero4 || !rock_gostar || !metal_gostar || !ouvir_semana) {
+        alert('Please fill in all the required fields.')
+        return;
+    }
+    storeValuesInLocalStorage();
+}
+
+function storeValuesInLocalStorage() {
+    // Access form elements and store their values in local storage
+    let formData = {
+        nome1: document.getElementById('nome1').value,
+        sobrenome2: document.getElementById('sobrenome2').value,
+        idade: document.getElementById('idade').value,
+        genero1: document.getElementById('Genero1').value,
+        genero2: document.getElementById('Genero2').value,
+        genero3: document.getElementById('Genero3').value,
+        genero4: document.getElementById('Genero4').value,
+        rock_gostar: document.getElementsByName('rock_gostar').value,
+        metal_gostar: document.getElementsByName('metal-gostar').value,
+        ouvir_semana: document.getElementsByName('ouvir_semana').value,
+    }
+
+    let timestamp = new Date().getTime(); // Generate a timestamp for a unique key
+
+    // Store data in local storage
+    localStorage.setItem(timestamp, JSON.stringify(formData))
+
+    // Optionally, you can clear the form after storing values
+    document.getElementById('questionnaireForm').reset()
+
+    alert('Form data has been successfully stored in local storage.')
+}
+
 function convertLocalStorageToObject() {
-    const localStorageData = {};
+    const localStorageData = {}
     // Loop through all items in LocalStorage
     for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
+        const key = localStorage.key(i)
+        const value = localStorage.getItem(key)
         // Parse the value if it's valid JSON
         try {
-            localStorageData[key] = JSON.parse(value);
+            localStorageData[key] = JSON.parse(value)
         } catch (e) {
-            localStorageData[key] = value;
+            localStorageData[key] = value
         }
     }
-    return localStorageData;
+    return localStorageData
 }
 
 function exportLocalStorageToJSON(filename) {
